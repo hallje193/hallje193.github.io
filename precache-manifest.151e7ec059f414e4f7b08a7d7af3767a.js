110493a7cfe4af28a1f52573bd54349310acbb2f
Jerry <hall.jerry9@gmail.com>
2020-04-07 23:54:58 -0700

    Updates

---
+++ precache-manifest.151e7ec059f414e4f7b08a7d7af3767a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be235f80f27d70e81e936c5e92d7525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5892a43718c87351ec4a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5545c240fd26810b7b3",
+    "url": "/static/js/2.c10dea83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c10dea83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5892a43718c87351ec4a",
+    "url": "/static/js/main.4fb4215d.chunk.js"
+  },
+  {
+    "revision": "68d61b3a7d8b02300f0c",
+    "url": "/static/js/runtime-main.2c90e96e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
